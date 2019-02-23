bd4addf7c0f4e292b494fe2be57065797dd80705
rafal-rumanek <rafal.rumanek@codewise.com>
2019-02-23 19:44:57 +0100

    crated step1 backup, .gitignore, minor improvements for low-res screens

---
+++ step_1/app.js
@@ -0,0 +1,30 @@
+addPostToHTML({
+    author: "Filip Mamcarczyk",
+    text: "Czasem nie mam czasu",
+    date: "20/02/2019, 21:12"
+});
+
+addPostToHTML({
+    author: "Rafal Rumanek",
+    text: "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    date: "20/02/2019, 21:00"
+});
+
+
+addPostToHTML({
+    author: "Krzysztof Mikuta",
+    text: "There are many variations of passages of Lorem Ipsum available",
+    date: "20/02/2019, 20:44"
+});
+
+addPostToHTML({
+    author: "Ada Rys",
+    text: "Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+    date: "20/02/2019, 19:12"
+});
+
+addPostToHTML({
+    author: "Kacper Sokolowski",
+    text: "All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.",
+    date: "20/02/2019, 18:02"
+});
