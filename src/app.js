e0e298b2dfaa9d6a9f37684f75a978eb35072bac
rafal-rumanek <rafal.rumanek@codewise.com>
2019-03-02 22:26:25 +0100

    added initial version of code for workshops, more relevant tweets

--- src/app.js
+++ src/app.js
@@ -1,43 +1,26 @@
 // API reference available at http://girlsjs.codewise.com:3000/api-docs
-const POSTS_ENDPOINT = "http://girlsjs.codewise.com:3000/api/posts";
+// const POSTS_ENDPOINT = "http://girlsjs.codewise.com:3000/api/posts";
 
-const newPostBtn = document.querySelector('.new-post-button');
-const contentArea = document.querySelector('.new-post-content');
+/* const posts = [{
+    text: "Ada Lovelace, angielska matematyczka i pisarka, jest uznawana za twórczynię pierwszego programu a zarazem pierwszą w historii programistkę.",
+    author: "Maria Italiano",
+    date: "28.02.2019, 16:22:15"
+}, {
+    text: "Według serwisu Statisa, szacuje się, że ruch mobilny stanowił w 2018 roku ok. 52.2% całego ruchu w Internecie. Ze względu na rosnący trend, można spodziewać się, że w 2019 ten odsetek będzie jeszcze większy.",
+    author: "Dionizy Złotopolski",
+    date: "27.02.2019, 12:59:20"
+}, {
+    text: "Najpopularniejsza obecnie przeglądarka Google Chrome powstała dopiero w drugiej połowie 2008 roku. Jej udział w rynku szacuje się na ok. 62%.",
+    author: "Barbara Mostowiak",
+    date: "25.02.2019, 08:13:44"
+}, {
+    text: "Codziennie pojawia się ponad 500 milinów nowych Tweetów ",
+    author: "Maurycy Vivaldi",
+    date: "25.02.2019, 06:26:01"
+}]; */
 
-newPostBtn.addEventListener('click', sendNewPost);
-
-fetchPosts();
-
-function sendNewPost() {
-    const content = {
-        author: 'Me',
-        text: contentArea.value
-    };
-
-    const reqObj = {
-        method: 'POST',
-        body: JSON.stringify(content),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
-
-    fetch(POSTS_ENDPOINT, reqObj)
-        .then(response => response.json())
-        .then(post => addPostToHTML(post))
-        .then(() => contentArea.value = '')
-        .catch(err => {
-            alert("Something went terribly wrong when trying to add a post!");
-            console.log(err);
-        });
-}
-
-function fetchPosts() {
-    fetch(POSTS_ENDPOINT)
-        .then(response => response.json())
-        .then(data => data.posts.forEach(addPostToHTML))
-        .catch(err => {
-            alert("Something went terribly wrong when trying to fetch posts!");
-            console.log(err);
-        });
-}
+addPostToHTML({
+    text: "Ada Lovelace, angielska matematyczka i pisarka, jest uznawana za twórczynię pierwszego programu a zarazem pierwszą w historii programistkę.",
+    author: "Maurycy Vivaldi",
+    date: "28.02.2019, 16:22:15"
+});
